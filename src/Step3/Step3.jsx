96d67e8953bfc77e9e51afa43407cd8a8bc2b8ff
Tanguy <tanguymorel.pro@gmail.com>
2025-05-20 09:34:47 +0200

    add: filtering a list in Step3 - Exercise2

--- src/Step3/Step3.jsx
+++ src/Step3/Step3.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Step3Rules from './Step3Rules'
 import Exercice1 from './components/Exercice1'
-
+import Exercice2 from './components/Exercice2'
 
 
 const Step3 = () => {
   return (
     <>
     <Step3Rules/>
-    <Exercice1/>
+    <Exercice2/>
     </>
     
   )
---
+++ src/Step3/components/Exercice2.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const Exercice2 = () => {
+
+    const films = [
+        { id: 1, title: 'Inception' },
+        { id: 2, title: 'Interstellar' },
+        { id: 3, title: 'The Matrix' },
+        { id: 4, title: 'Avatar' },
+    ];
+
+    const [searchItem, setSearchItem] = useState("")
+    function handleChange(e) {
+        setSearchItem(e.target.value)
+    }
+
+    const filteredList = films.filter(film =>
+        film.title.toLowerCase().includes(searchItem.toLowerCase())
+    );
+
+
+
+    return (
+        <>
+            <input type="text" onChange={handleChange} />
+            <ul>
+                {filteredList.map(film => (
+                    <li key={film.id}>
+                        {film.title}
+                    </li>
+
+                ))
+                }
+            </ul>
+        </>
+    )
+}
+
+export default Exercice2
